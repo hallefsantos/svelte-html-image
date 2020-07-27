b9a95c3092ef4d059f57d080b61dcf2a7fe84e74
Hallef Santos <hallefsantos@gmail.com>
2020-07-27 11:43:15 -0300

    remove html2canvas and using only canvas api now

--- src/components/GenerateImage.svelte
+++ src/components/GenerateImage.svelte
@@ -1,42 +1,79 @@
 <script context="module">
-   import html2canvas from 'html2canvas';
-   let phraseEl;
-   export const generateCanvas = () => {
-      setTimeout(() => {
-         html2canvas(phraseEl).then(function(canvas) {
+   let canvasEl;
+   let phraseBg;
 
-            if(window.navigator.msSaveBlob) {
-               window.navigator.msSaveBlob(canvas.msToBlob(), "phrase-image.png")
-            } else {
-               const a = document.createElement('a');
-               document.body.appendChild(a);
-               a.href = canvas.toDataURL();
-               a.download = 'phrase-image.png';
-               a.click();
-               document.body.removeChild(a);
-            }
-         });
-      }, 100);
+   export const generateCanvas = (phrase) => {   
+      const ctx = canvasEl.getContext("2d");
+      const maxWidth = 400;
+      const primary = phrase.portuguese;
+      const secondary = phrase.portuguese_en;
+      const x = (canvasEl.width - maxWidth) / 2;
+
+      ctx.drawImage(phraseBg, 0, 0);
+      ctx.font = "36px Helvetica";
+      ctx.fillStyle = '#111';
+
+      wrapText(ctx, primary, x, 220, maxWidth, 45);
+      
+      ctx.font = "26px Helvetica";
+      ctx.fillStyle = '#111';
+
+      wrapText(ctx, secondary, x, (270 +(36*getLines(ctx, primary, (maxWidth - 100)).length)), maxWidth, 35);
+      generateLink();
+   }
+
+   const generateLink = () => {
+      if(window.navigator.msSaveBlob) {
+         window.navigator.msSaveBlob(canvasEl.msToBlob(), "phrase-image.png")
+      } else {
+         const a = document.createElement('a');
+         document.body.appendChild(a);
+         a.href = canvasEl.toDataURL();
+         a.download = 'phrase-image.png';
+         a.click();
+         document.body.removeChild(a);
+      }
+   }
+
+   function wrapText(context, text, x, y, maxWidth, lineHeight) {
+      const words = text.split(' ');
+      let line = '';
+
+      for (let n = 0; n < words.length; n++) {
+         const testLine = line + words[n] + ' ';
+         const metrics = context.measureText(testLine);
+         const testWidth = metrics.width;
+         if (testWidth > maxWidth && n > 0) {
+            context.fillText(line, x, y);
+            line = words[n] + ' ';
+            y += lineHeight;
+         } else {
+            line = testLine;
+         }
+      }
+      context.fillText(line, x, y);
+   }
+
+   function getLines(ctx, text, maxWidth) {
+      var words = text.split(" ");
+      var lines = [];
+      var currentLine = words[0];
+
+      for (var i = 1; i < words.length; i++) {
+         var word = words[i];
+         var width = ctx.measureText(currentLine + " " + word).width;
+         console.log(maxWidth);
+         if (width < maxWidth) {
+            currentLine += " " + word;
+         } else {
+            lines.push(currentLine);
+            currentLine = word;
+         }
+      }
+      lines.push(currentLine);
+      return lines;
    }
 </script>
 
-<script>
-   export let phrase = '';
-   export let state = false;
-</script>
-
-<style>
-   .frase {
-      width: 600px;
-      height: 600px;
-      opacity: 1;
-   }
-</style>
-
-<div bind:this="{phraseEl}" class="{state ? 'block' : 'hidden' } border border-gray-300 p-5 frase flex items-center justify-center flex-col relative">
-   <img class="absolute inset-0" src="frase.jpg" alt="">
-   <div class="relative z-10 max-w-sm text-gray-800">
-      <h2 class="text-4xl mb-5">{phrase.portuguese}</h2>
-      <p class="text-2xl">{phrase.portuguese_en}</p>
-   </div>
-</div>
+<canvas bind:this={canvasEl}  class="hidden" width="600" height="600"></canvas>
+<img bind:this={phraseBg} class="hidden" src="frase.jpg">
--- src/components/PhrasesList.svelte
+++ src/components/PhrasesList.svelte
@@ -2,53 +2,45 @@
    import Card from './Card.svelte';
    import GenerateImage, { generateCanvas } from './GenerateImage.svelte';
 
-   let generateImagePhrase = '';
-
    const phrases = [
       {
-         portuguese: 'A esperança é a última que morre.', 
-         portuguese_en: "While there's life, there's hope."
+         portuguese: 'De grão em grão a galinha enche o papo.', 
+         portuguese_en: "Little by little the bird builds its nest."
       },
       {
          portuguese: 'A união faz a força.', 
          portuguese_en: "Many hands make light work."
       },
       {
-         portuguese: 'Quando um não quer, dois não brigam.', 
-         portuguese_en: "It takes two to tango."
+         portuguese: 'A grama da vizinho é sempre mais verde.', 
+         portuguese_en: "The grass is always greener on the other side of the fence."
       },
       {
-         portuguese: 'Vivendo e aprendendo.', 
-         portuguese_en: "You live and learn."
+         portuguese: 'Não deixe para amanhã o que você pode fazer agora.', 
+         portuguese_en: "Never put off till tomorrow what you can do today."
       },
       {
-         portuguese: 'Saco vazio não para em pé.', 
-         portuguese_en: "An empty bag won't stand."
+         portuguese: 'Água mole em pedra dura tanto bate até que fura.', 
+         portuguese_en: "Water dropping day by day wears the hardest rock away."
       },
       {
-         portuguese: 'Rir é o melhor remédio.', 
-         portuguese_en: "Laughter is the best medicine."
+         portuguese: 'Cada um por si e Deus por todos.', 
+         portuguese_en: "Every man for himself (and the devil take the hindmost)."
       },
    ];
 
    let generateState = false;
 
    const clicou = frase => {
-      console.log(frase);
-      generateImagePhrase = frase;
-      generateCanvas();
-      generateState = true;
+      generateCanvas(frase);
    }
 
 </script>
 
-<section class="div flex flex-wrap align-top py-20">
+<section class="div flex flex-wrap align-top py-10">
    {#each phrases as phrase}
       <Card on:click={() => clicou(phrase) } {phrase} />
    {/each}
-
-   <GenerateImage state={generateState}  phrase={generateImagePhrase} />
 </section>
-<style>
-   
-</style>
+
+<GenerateImage />
